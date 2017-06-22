92d732aa4dd9877c8aa3ef58056269abce713c67
= <undertakers_help@yahoo.com>
2017-06-22 18:15:20 +0200

    Var for Stacksize inserted

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
-minetest.craftitemdef_default.stack_max=1024
-minetest.nodedef_default.stack_max=1024
-minetest.noneitemdef_default.stack_max=1024
+local stack_size = 1024
+
+minetest.craftitemdef_default.stack_max=stack_size
+minetest.nodedef_default.stack_max=stack_size
+minetest.noneitemdef_default.stack_max=stack_size
