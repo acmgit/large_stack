8ca5051de15517e0aa1cea2d2aa4af7e299776bc
tacotexmex <josefsahlberg+minetest@gmail.com>
2017-07-13 11:43:22 +0200

    Global setting var

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
-local stack_size = 1024
+local stack_size = or tonumber(minetest.settings:get("stack_size")) or 1024
 
-minetest.craftitemdef_default.stack_max=stack_size
-minetest.nodedef_default.stack_max=stack_size
-minetest.noneitemdef_default.stack_max=stack_size
+minetest.craftitemdef_default.stack_max = stack_size
+minetest.nodedef_default.stack_max = stack_size
+minetest.noneitemdef_default.stack_max = stack_size
