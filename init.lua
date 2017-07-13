689e60096f7aebf409aedb8d3df07ce98cc68b3d
tacotexmex <josefsahlberg+minetest@gmail.com>
2017-07-13 11:45:57 +0200

    Typo

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-local stack_size = or tonumber(minetest.settings:get("stack_size")) or 1024
+local stack_size = tonumber(minetest.settings:get("stack_size")) or 1024
 
 minetest.craftitemdef_default.stack_max = stack_size
 minetest.nodedef_default.stack_max = stack_size
